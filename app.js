417ff41f6f524b554b630337701ac614a4b2540c
shivam kumar <shivamkr19102000@gmail.com>
2023-07-16 00:42:59 +0530

    fixed

--- app.js
+++ app.js
@@ -12,8 +12,8 @@ app.use(cors({
 
 
 // middlewares 
-app.use(express.json({limit: "50mb"}));
-app.use(urlencoded({extended: true,limit:"50mb"}));
+app.use(express.json());
+app.use(urlencoded({extended: true}));
 app.use(cookieParser());
 
 // routes
